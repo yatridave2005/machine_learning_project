b31512f1c2ee80e21e908c571451fc07a0b60119
yatridave2005 <jagrutidave789@gmail.com>
2026-01-28 20:36:47 +0530

    Remove StandardScaler usage in SVC model training
    Removed scaling of training and test data for model fitting.

--- ml.py
+++ ml.py
@@ -3,7 +3,7 @@ from sklearn.svm import SVC
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 import pickle
-from sklearn.preprocessing import StandardScaler
+
 
 
 
@@ -19,9 +19,7 @@ y = df["Disease"]
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
-scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
+
 
 model = SVC(
     C=1.0,
@@ -30,14 +28,14 @@ model = SVC(
     gamma="scale",
 )
 
-model.fit(X_train_scaled, y_train)
+model.fit(X_train, y_train)
 
 print("Accuracy:", model.score(X_test, y_test))
 
 
 pickle.dump(model, open("disease_model.pkl", "wb"))
 pickle.dump(le, open("label_encoder.pkl", "wb"))
-pickle.dump(scaler, open("scaler.pkl", "wb"))
+
 
 print("model saved successfully")
 
